cd455268787695cbb60d8390b23e50e7cbaa2215
AJ <drlimeproject@gmail.com>
2021-01-02 00:51:05 -0500

    Initial Comment

---
+++ Quiz App/Quiz App/Quiz_App.h
@@ -0,0 +1,18 @@
+//
+//  Quiz_App.h
+//  Quiz App
+//
+//  Created by Aeman Jamali on 1/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Quiz_App.
+FOUNDATION_EXPORT double Quiz_AppVersionNumber;
+
+//! Project version string for Quiz_App.
+FOUNDATION_EXPORT const unsigned char Quiz_AppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Quiz_App/PublicHeader.h>
+
+
